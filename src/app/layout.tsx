035d600bee01e219ce54eda84ff5b7a93d182297
Anderson T <tseng.andersonn@gmail.com>
2025-10-14 23:44:12 -0700

    feat: Add @vercel/analytics for enhanced analytics tracking in the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@xyflow/react/dist/style.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
+      <Analytics />
     </html>
   );
 }
